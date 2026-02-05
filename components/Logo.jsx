7b333ee46d5342ad9f6efda4e02f0f4560edc67e
Isa Sloot <isasloot@hotmail.com>
2026-02-05 18:23:50 +0000

    fix image extension for EAS build

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -4,7 +4,7 @@ import { Image } from "react-native";
 export default function Logo({ size = 40 }) {
   return (
     <Image
-      source={require("../assets/kura-logo")}
+      source={require("../assets/kura-logo.png")}
       style={{ width: size, height: size }}
       resizeMode="contain"
     />
