5a054ccb6ab870634ab62871ef60cf4bb4a0e13e
Isa Sloot <isasloot@hotmail.com>
2026-02-05 17:00:03 +0000

    changes to logo

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -4,7 +4,7 @@ import { Image } from "react-native";
 export default function Logo({ size = 40 }) {
   return (
     <Image
-      source={require("../assets/kura-logo.png")}
+      source={require("../assets/kura-logo")}
       style={{ width: size, height: size }}
       resizeMode="contain"
     />
